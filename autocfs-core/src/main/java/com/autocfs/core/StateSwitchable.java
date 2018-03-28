70e23183ad96e691680773a6441b5c46c5e3946a
Codimiracle <Codimiracle@outlook.com>
2018-03-28 11:13:01 +0800

    autocfs-client project update 2018-03-28 11:12

--- autocfs-core/src/main/java/com/autocfs/core/StateSwitchable.java
+++ autocfs-core/src/main/java/com/autocfs/core/StateSwitchable.java
@@ -1,15 +1,21 @@
 package com.autocfs.core;
 
-import javax.naming.OperationNotSupportedException;
-
 public interface StateSwitchable {
     public void start() throws UnsupportedOperationException;
+
     public void pause() throws UnsupportedOperationException;
+
     public void resume() throws UnsupportedOperationException;
+
     public void stop() throws UnsupportedOperationException;
+
     public void restart() throws UnsupportedOperationException;
+
     public boolean isStarted();
+
     public boolean isPaused();
+
     public boolean isStopped();
+
     public boolean isFinished();
 }
